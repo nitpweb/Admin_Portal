32802e360dd8c1c584a1d4ba98c0dc23ad6c7dc4
manishkr1101 <manishkumarsh120@gmail.com>
2020-06-22 23:28:54 +0530

    update db connection

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const http = require('http')
 require('dotenv').config()
 const app = require('./app')
 
+require('./db')
+
+
 const PORT = process.env.PORT | 3000
+
 app.listen(PORT, () => {
     console.log(`app is running on port : ${PORT}`)
 })
