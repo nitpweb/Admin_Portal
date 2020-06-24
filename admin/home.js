64423290b33790f05307cd27eb3702ddbea81e0a
kundannitp <kundan3316@gmail.com>
2020-06-24 22:30:20 +0530

    front-end start

--- admin/home.js
+++ admin/home.js
@@ -3,7 +3,10 @@ var formidable = require('formidable');
 var fs = require('fs');
 
 router.get('/', (req, res) => {
-    res.render('index',{})
+    res.render('home', {
+        profileimgsrc: 'images/profiledefault.jfif',
+        title_top:'Home'
+    })
 })
 
 module.exports = router
