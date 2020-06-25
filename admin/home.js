23d48b36b260557122b97e8eeab12e895d37750a
kundannitp <kundan3316@gmail.com>
2020-06-25 17:50:57 +0530

    KKj commit

--- admin/home.js
+++ admin/home.js
@@ -4,8 +4,7 @@ var fs = require('fs');
 
 router.get('/', (req, res) => {
     res.render('home', {
-        profileimgsrc: 'images/profiledefault.jfif',
-        title_top:'Home'
+        
     })
 })
 
