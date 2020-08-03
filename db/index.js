0feddc76f69530b0b503ad9d99d3d648e7bb9093
kundannitp <kundan3316@gmail.com>
2020-08-04 02:07:22 +0530

    before pull

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql')
 const e = require('express')
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST | 'localhost',
-//     port: process.env.DB_PORT | 3306,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// })
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST | 'localhost',
+    port: process.env.DB_PORT | 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 
 connection.on('error', function(err) {
     console.log(err.code)
