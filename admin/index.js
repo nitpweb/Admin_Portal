dc90e309c069c6070ae613007aa91af4d6e4b111
Divya Prakash <medivyaprakash@gmail.com>
2020-11-26 02:13:24 +0530

    events auth added

--- admin/index.js
+++ admin/index.js
@@ -1,92 +1,96 @@
-const router = require('express').Router()
-var formidable = require('formidable');
-var fs = require('fs');
+const router = require("express").Router();
+var formidable = require("formidable");
+var fs = require("fs");
 
-const session = require('express-session');
+const session = require("express-session");
 
-router.use(session({
+router.use(
+  session({
     secret: "ablackcat",
     resave: false,
     saveUninitialized: true,
-}));
+  })
+);
 
-router.get('/', (req, res) => {
-    res.redirect('/profile')
-})
+router.get("/", (req, res) => {
+  res.redirect("/profile");
+});
 
 // Handling events route
-const eventRouter = require('./routes/events')
-router.use('/events', eventRouter)
+const eventRouter = require("./routes/events");
+router.use("/events", eventRouter);
 
+const noticeRouter = require("./routes/notices");
+router.use("/notices", noticeRouter);
 
-const noticeRouter = require('./routes/notices')
-router.use('/notices', noticeRouter)
+const newsRouter = require("./routes/news");
+router.use("/news", newsRouter);
 
+const profileRouter = require("./routes/faculty_prof");
+router.use("/profile", profileRouter);
 
-const profileRouter = require('./routes/faculty_prof')
-router.use('/profile', profileRouter)
+const googlelogin = require("./routes/googlesign");
+router.use("/googlesign", googlelogin);
 
-const googlelogin = require("./routes/googlesign")
-router.use('/googlesign', googlelogin)
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-router.get('/login', (req, res) => {
-    res.render('login')
-})
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  req.session = null;
+  res.redirect("/login");
+});
 
-router.get('/logout', (req, res) => {
-    req.session.destroy()
-    req.session = null;
-    res.redirect("/login")
-})
+const changeDrive = require("./routes/changeDrive");
+router.use("/changeDrive", changeDrive);
 
-const changeDrive = require("./routes/changeDrive")
-router.use('/changeDrive', changeDrive)
+const newNotices = require("./routes/newNotices");
+router.use("/newNotices", newNotices);
 
-const newNotices = require("./routes/newNotices")
-router.use('/newNotices', newNotices)
+const uploadNotices = require("./routes/uploadNotice");
+router.use("/uploadNotices", uploadNotices);
 
-const uploadNotices = require("./routes/uploadNotice")
-router.use('/uploadNotices', uploadNotices)
+const uploadNews = require("./routes/uploadNews");
+router.use("/uploadNews", uploadNews);
 
-const saving_folder_id = require("./routes/saving_folder_id")
-router.use('/saving_folder_id', saving_folder_id)
+const saving_folder_id = require("./routes/saving_folder_id");
+router.use("/saving_folder_id", saving_folder_id);
 
-const uploadEvents = require("./routes/events/uploadEvents")
-router.use('/uploadEvents', uploadEvents)
+const uploadEvents = require("./routes/events/uploadEvents");
+router.use("/uploadEvents", uploadEvents);
 
-const uploadBib = require("./routes/uploadBib")
-router.use('/uploadBib', uploadBib)
+const uploadBib = require("./routes/uploadBib");
+router.use("/uploadBib", uploadBib);
 
-const facManagementRouter = require('./routes/faculty-management')
-router.use('/faculty-management', facManagementRouter)
+const facManagementRouter = require("./routes/faculty-management");
+router.use("/faculty-management", facManagementRouter);
 
-const addSubject = require("./routes/faculty-data/add_subjects")
-router.use('/add-subject', addSubject)
+const addSubject = require("./routes/faculty-data/add_subjects");
+router.use("/add-subject", addSubject);
 
-const addphdcandidates = require("./routes/faculty-data/add_phdcandidates")
-router.use('/add-phdcandidates', addphdcandidates)
+const addphdcandidates = require("./routes/faculty-data/add_phdcandidates");
+router.use("/add-phdcandidates", addphdcandidates);
 
-const addnewproject = require("./routes/faculty-data/add_newproject")
-router.use('/add-newproject', addnewproject)
+const addnewproject = require("./routes/faculty-data/add_newproject");
+router.use("/add-newproject", addnewproject);
 
-const addnewprofessionalservice = require("./routes/faculty-data/add_professionalservice")
-router.use('/add-professional-service', addnewprofessionalservice)
+const addnewprofessionalservice = require("./routes/faculty-data/add_professionalservice");
+router.use("/add-professional-service", addnewprofessionalservice);
 
-const addworkexperience = require("./routes/faculty-data/add_workexperience")
-router.use('/add-work-experience', addworkexperience)
+const addworkexperience = require("./routes/faculty-data/add_workexperience");
+router.use("/add-work-experience", addworkexperience);
 
+const memberships = require("./routes/faculty-data/add_memberships");
+router.use("/memberships", memberships);
 
-const memberships = require("./routes/faculty-data/add_memberships")
-router.use('/memberships', memberships)
+const education = require("./routes/faculty-data/add_education");
+router.use("/education", education);
 
-const education = require("./routes/faculty-data/add_education")
-router.use('/education', education)
+const currResponsibility = require("./routes/faculty-data/add_curr_responsibility");
+router.use("/currResponsibility", currResponsibility);
 
-const currResponsibility = require("./routes/faculty-data/add_curr_responsibility")
-router.use('/currResponsibility', currResponsibility)
+const pastResponsibility = require("./routes/faculty-data/add_past_responsibility");
+router.use("/pastResponsibility", pastResponsibility);
 
-const pastResponsibility = require("./routes/faculty-data/add_past_responsibility")
-router.use('/pastResponsibility', pastResponsibility)
-
-
-module.exports = router
+module.exports = router;
